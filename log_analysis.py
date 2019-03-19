c019c982b0c715c7c5fdbb595d723bfe7778bb83
Redsnip8 <FCipolone@gmail.com>
2019-03-19 17:54:10 -0400

    Version 1.0 of python log aplication

---
+++ log_analysis.py
@@ -0,0 +1,63 @@
+import psycopg2
+import time
+import os
+
+#Declare variable for Database Name
+DBNAME = "news"
+
+#Get the 3 most popular posts
+def get_top_articles():
+    conn = psycopg2.connect(dbname=DBNAME)
+    cur = conn.cursor()
+    cur.execute("select title, count from articleviewcount order by count desc limit 3;")
+    top_posts = cur.fetchall()
+    conn.close()
+    return top_posts
+
+#Get the 3 most popular authors
+def get_top_authors():
+    conn = psycopg2.connect(dbname=DBNAME)
+    cur = conn.cursor()
+    cur.execute("select name, sum from authorviewcount order by sum desc limit 3;")
+    top_authors = cur.fetchall()
+    conn.close()
+    return top_authors
+
+#Get days with >1% error codes
+def get_high_error_day():
+    conn = psycopg2.connect(dbname=DBNAME)
+    cur = conn.cursor()
+    cur.execute("select day, error_percent from dailystatuslog where error_percent > 1 order by day;")
+    error_log = cur.fetchall()
+    conn.close()
+    return error_log
+
+#Write data to a txt file
+
+#Variables for gets
+toparticles = get_top_articles()
+topauthors = get_top_authors()
+errors = get_high_error_day()
+
+#variable for time and date stamp
+timestmp = time.strftime("%Y%b%d_%H.%M_%Z")
+#Get CWD as variable
+cwd = os.getcwd()
+#Create a new report fiel with time stamp in the log folder under the News directory
+report = open(cwd + "/reports/newsReport_" + timestmp + ".txt", "w+")
+#write the top three articles, top three authors, list of days with over 1% errors with two new lines between them
+report.write("Top Articles and View Count\r\n")
+for article in toparticles:
+   report.write("Article: " + article[0] + "\n  Views: " + str(article[1]) + "\r\n")
+
+report.write("\n\nTop Authors and View Count\r\n")
+
+for author in topauthors:
+    report.write("Author: " + author[0] + "\n Views: " + str(author[1]) + "\r\n")
+
+report.write("\n\nDays With Page Request Errors over 1%\r\n")
+for day in errors:
+     report.write("Day: " + str(day[0]) + "\n Error Percentage: " + str(day[1]) +"%\r\n")
+#save the new report file
+report.seek
+report.close()
