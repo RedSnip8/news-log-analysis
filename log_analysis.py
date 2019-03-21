d6dc3be4dbef02a3b3b096bb9c869bb6c2abdaf0
Redsnip8 <FCipolone@gmail.com>
2019-03-21 17:13:19 -0400

    Refactored code, removing redudancy in methods and variable call

--- log_analysis.py
+++ log_analysis.py
@@ -7,52 +7,19 @@ import os
 # Declare variable for Database Name
 DBNAME = "news"
 
+#variable for database queries for:
 
-def get_top_articles():
-    # Get the 3 most popular posts
-    conn = psycopg2.connect(dbname=DBNAME)
-    cur = conn.cursor()
-    cur.execute(
-        """select title, count from articleviewcount
+# -top artiles using established view
+artilces_query = """select title, count from articleviewcount
         order by count desc limit 3;"""
-        )
-    top_posts = cur.fetchall()
-    conn.close()
-    return top_posts
 
+# -top authors using estblished view
+authors_query =  """select name, sum from authorviewcount
+        order by sum desc limit 3;"""
 
-def get_top_authors():
-    # Get the 3 most popular authors
-    conn = psycopg2.connect(dbname=DBNAME)
-    cur = conn.cursor()
-    cur.execute(
-        """select name, sum from authorviewcount
-        order by sum desc limit 3;""""
-        )
-    top_authors = cur.fetchall()
-    conn.close()
-    return top_authors
-
-
-def get_high_error_day():
-    # Get days with >1% error codes
-    conn = psycopg2.connect(dbname=DBNAME)
-    cur = conn.cursor()
-    cur.execute(
-        """select day, error_percent from dailystatuslog
-        where error_percent > 1 order by day;""""
-        )
-    error_log = cur.fetchall()
-    conn.close()
-    return error_log
-
-
-# Write data to a txt file
-
-# Variables for gets
-toparticles = get_top_articles()
-topauthors = get_top_authors()
-errors = get_high_error_day()
+# -days with returned data errors over 1%
+errorPerc_query = """select day, error_percent from dailystatuslog
+        where error_percent > 1 order by day;"""
 
 # variable for time and date stamp
 timestmp = time.strftime("%Y%b%d_%H.%M_%Z")
@@ -60,6 +27,23 @@ timestmp = time.strftime("%Y%b%d_%H.%M_%Z")
 cwd = os.getcwd()
 # Create a new file with time stamp in log folder
 report = open(cwd + "/reports/newsReport_" + timestmp + ".txt", "w+")
+
+
+def execute_query(sql_code):
+    conn = psycopg2.connect(dbname=DBNAME)
+    cur = conn.cursor()
+    cur.execute(sql_code)
+    data = cur.fetchall()
+    conn.close()
+    return data
+
+# Write data to a txt file
+
+# Variables for gets
+toparticles = execute_query(artilces_query)
+topauthors = execute_query(authors_query)
+errors = execute_query(errorPerc_query)
+
 # write each chunk of data with two new lines between them
 report.write("Top Articles and View Count\r\n")
 for article in toparticles:
